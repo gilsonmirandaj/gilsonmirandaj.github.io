c5f0dd24752fa02da67cd1ce4930494d8c4ff653
gilsonmirandaj <gilsonmirandajunior2@gmail.com>
2021-06-18 12:49:12 -0300

    darkmode

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+
+});
+
+function func()
+{
+    document.getElementById("ptext").style.color="red";
+}
